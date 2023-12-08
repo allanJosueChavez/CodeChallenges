6a4242c6f1d12b4f1d4e61fed9a70dfa157d9750
Allan Josue Chavez Sin <infodesarrollo12@minemgt.local>
2023-12-08 14:56:17 -0600

    feat: day 8 tried to count till the every starting point gets to a point that ends with 'Z'

--- AdventOfCode/Day8/index.js
+++ AdventOfCode/Day8/index.js
@@ -39,13 +39,13 @@ function calculateStepTillReachGoal(){
 function buildMap(mapElements){
     mapElements.shift() // remove break line
     let map = mapElements.map((element) => {
-        const LETTERSREGEX = /[A-Z]/g
+        const LETTERSANDNUMBERS = /[A-Z0-9]/g
         const elementKey = element.split("=")[0].trim()
         // console.log(element)
         const point = {
             key: elementKey,
-            L:  element.split("=")[1].split(",")[0].trim().match(LETTERSREGEX).join(""),
-            R:  element.split("=")[1].split(",")[1].trim().match(LETTERSREGEX).join(""),
+            L:  element.split("=")[1].split(",")[0].trim().match(LETTERSANDNUMBERS).join(""),
+            R:  element.split("=")[1].split(",")[1].trim().match(LETTERSANDNUMBERS).join(""),
         }
         return point
     })
@@ -63,40 +63,50 @@ function calculateStepsToSpecifiedNodes(){
     instructionsData.shift() // Remove the directions
     const mapElements = instructionsData
     const map = buildMap(mapElements)
-    let stepsCounter = 0
+
     const startingNodes = map.filter((point) => point.key.endsWith('A'))
     console.log("Starting nodes: ", startingNodes)
     let nextSteps = []
-    let goalReached = false
-    
+
+    let stepsPerNode = [] // Array to save the number of steps where each node reached a point key that ends in Z
     directions.pop()  
-    do{
-        directions.forEach((direction, index) => {
-            if(!goalReached){
-                let currentPoints = stepsCounter === 0 ?  startingNodes : nextSteps
-                const indicator = direction === "L" ? "L" : "R"
-                console.log("indicator: ", direction)
-                nextSteps = []
-                currentPoints.forEach((currentPoint) => {
-                    const nextStep = map.find((point) => point.key === currentPoint[indicator])
-                    nextSteps.push(nextStep)
-                })
-                console.log("Next steps: ", nextSteps)
-                stepsCounter++
-                // pointsEndInZ is a boolean value if all the keys of nextSteps end in Z
-                const pointsEndInZ = nextSteps.every((point) => point.key.endsWith('Z'))
-                console.log("Points end in Z: ", pointsEndInZ)
-                
-                if(pointsEndInZ){
-                    goalReached = true
-                    console.log("Reached the goal at step: ", stepsCounter)
-                    return
-                } 
-           
-            }
+
             
-        })
-    }while(!goalReached)
+               
+    do{
+       
+                startingNodes.forEach((startingPoint) => {
+                    let stepsCounter = 0
+                    let goalReached = false
+                    let nextStepPerNode = null
+                    const currentPoint = stepsCounter === 0 ? startingPoint : nextStepPerNode
+        console.log("Current point: ", currentPoint)
+
+                        directions.forEach((direction) => {
+                            if(!goalReached){
+                                console.log("Direction: ", direction)
+                                nextStepPerNode = map.find((point) => point.key === currentPoint[direction])
+                                console.log("Next step: ", nextStepPerNode)
+                                stepsCounter++
+                                if(nextStepPerNode.key.endsWith('Z')){
+                                    goalReached = true
+                                    console.log("Reached the goal at step: ", stepsCounter)
+                                    stepsPerNode.push(stepsCounter)
+                                    stepsCounter = 0
+                                    return
+                                }
+                            }
+
+                })
+
+            })
+        }while(!goalReached)
+                //stepsCounter++
+                // pointsEndInZ is a boolean value if all the keys of nextSteps end in Z
+                // const pointsEndInZ = nextSteps.every((point) => point.key.endsWith('Z'))
+                // I can use a MCM to calculate the total steps to reach all the nodes that end in Z instead of 
+           
+
     console.log("The steps to reach all nodes that end in Z: ", stepsCounter)
 }
 calculateStepsToSpecifiedNodes()
